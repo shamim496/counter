55b70054fd5c5bfb13974179bd3410f6410aba92
Shamim Hasan <132350550+shamim496@users.noreply.github.com>
2023-10-14 13:07:19 +0600

    Add files via upload

---
+++ counter/js/script.js
@@ -0,0 +1,72 @@
+// // set inital count
+// let count = 0;
+
+// //select value and buttons
+// const value = document.querySelector("#value");
+// const btns = document.querySelectorAll(".btn");
+
+// btns.forEach(function (btn) {
+//     btn.addEventListener("click", function (s) {
+//       const style = s.currentTarget.classList;
+//         if (style.contains('decrease')){
+//         count--;
+//       }else if(style.contains('increase')){
+//         count++;
+//       }else{
+//         count = 0;
+//       }
+//       if(count > 0){
+//         value.style.color = "green";
+//       }else if(count < 0){
+//         value.style.color = "red";
+//       }
+//       if(count === 0){
+//         value.style.color = "blue";
+//       }
+//     value.textContent = count;
+//     });
+// });
+
+
+
+// let count = 0;
+
+// const value = document.querySelector("#value");
+// const btns = document.querySelectorAll(".btn");
+
+// btns.forEach(function (btn){
+//     btn.addEventListener("click",function(s){
+//         const style = s.currentTarget.classList;
+//         if(style.contains('decrease')){
+//             count--;
+//         }else if (style.contains('increase')){
+//             count++
+//         }else{
+//             count = 0;
+//         }
+//         if(count > 0){
+//             value.style.color = "blue";
+//         }else if (count < 0){
+//             value.style.color = "green";
+//         }else{
+//             value.style.color = "red";
+//         }
+//         if (count === 0){
+//             value.style.color = "orange";
+//         }
+//         value.textContent = count;
+//     });
+// });
+
+
+
+let count  = 0;
+
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+    btns.addEventLister("click", function (s) {
+        const style = s.
+    });
+});
